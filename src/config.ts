e2da4b27b057672793fc4f297b4299d62c1b11c6
dapt <diegodapt18@gmail.com>
2023-11-06 09:46:07 +0100

    updating config

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import dotenv from "dotenv";
 dotenv.config()
 
 export default {
-  MONGO_DATABASE:process.env.MONGO_DATABASE || "videodb",
-  MONGO_USER:process.env.MONGO_USER || "admin",
-  MONGO_PASWORD:process.env.MONGO_PASWORD || "admin",
-  MONGO_HOST:process.env.MONGO_HOST || "localhost",
+  MONGO_DATABASE:process.env.MONGO_DATABASE,
+  MONGO_USER:process.env.MONGO_USER,
+  MONGO_PASWORD:process.env.MONGO_PASWORD,
+  MONGO_HOST:process.env.MONGO_HOST,
   PORT: process.env.PORT || 3000
 };
